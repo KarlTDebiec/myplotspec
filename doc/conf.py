53eef60e2270b107616a5b3b7ba6cb73d11e584c
Karl Debiec <karl.t.debiec@gmail.com>
2016-06-27 17:27:58 -0400

    working on github-pages

--- doc/conf.py
+++ doc/conf.py
@@ -1,86 +1,38 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+#   myplotspec/doc/conf.py
+#
+#   Copyright (C) 2015-2016 Karl T Debiec
+#   All rights reserved.
+#
+#   This software may be modified and distributed under the terms of the
+#   BSD license. See the LICENSE file for details.
+################################### MODULES ###################################
 import sphinx_rtd_theme
-
+################################ CONFIGURATION ################################
 todo_include_todos = False
 
 needs_sphinx = '1.3'
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.todo']
-# templates_path = ['_templates']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo' ]
 source_suffix = '.rst'
 source_encoding = 'utf-8'
 napoleon_use_rtype = False
 
 master_doc = 'index'
-project = "MYPlotSpec"
-copyright = "2015, Karl T Debiec"
-version = "0.1"
-release = "0.1"
+project   = "myplotspec"
+copyright = "2015-2016, Karl T Debiec"
+author    = "Karl T Debiec"
+version   = "0.1"
+release   = "0.1"
 
-# language = None
-# today = ''
-# today_fmt = '%B %d, %Y'
-exclude_patterns = ['_build']
-# default_role = None
-# add_function_parentheses = True
-# add_module_names = True
-# show_authors = False
-pygments_style = "sphinx"
-# modindex_common_prefix = []
-
-html_theme = "sphinx_rtd_theme"
-# html_theme_options = {}
-html_theme_path = sphinx_rtd_theme.get_html_theme_path()
-# html_title = None
-# html_short_title = None
-# html_logo = None
-# html_favicon = None
-html_static_path = ['_static']
-# html_last_updated_fmt = '%b %d, %Y'
-# html_use_smartypants = True
-# html_sidebars = {}
-# html_additional_pages = {}
-# html_domain_indices = True
-# html_use_index = True
-# html_split_index = False
-# html_show_sourcelink = True
-# html_show_sphinx = True
-# html_show_copyright = True
-# html_use_opensearch = ''
-# html_file_suffix = None
-htmlhelp_basename = "MYPlotSpecdoc"
-
-latex_elements = {
-"preamble": "\setcounter{tocdepth}{4}",
-}
-latex_use_modindex = False
-latex_documents = [
-  ("index", "MYPlotSpec.tex", "MYPlotSpec Documentation",
-   "Karl T Debiec", "manual"),
-]
-# latex_logo = None
-# latex_use_parts = False
-# latex_show_pagerefs = False
-# latex_show_urls = False
-# latex_appendices = []
-# latex_domain_indices = True
-
-# man_pages = [
-#     ('index', 'MYPlotSpec', u'MYPlotSpec Documentation',
-#      [u'Karl Debiec'], 1)
-# ]
-# man_show_urls = False
-
-# texinfo_documents = [
-#   ('index',
-#    'MYPlotSpec',
-#    'MYPlotSpec Documentation',
-#    'Karl Debiec',
-#    'MYPlotSpec',
-#    'One line description of project.',
-#    'Miscellaneous'),
-# ]
-# texinfo_appendices = []
-# texinfo_domain_indices = True
-# texinfo_show_urls = 'footnote'
-
-# intersphinx_mapping = {'http://docs.python.org/': None}
+exclude_patterns  = ["_build"]
+pygments_style    = "sphinx"
+html_theme        = "sphinx_rtd_theme"
+html_theme_path   = sphinx_rtd_theme.get_html_theme_path()
+html_static_path  = ["_static"]
+htmlhelp_basename = "myplotspecdoc"
