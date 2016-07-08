99690476e5d1c11f7c21643350ca8f0bda8b4aec
Karl Debiec <karl.t.debiec@gmail.com>
2016-07-08 10:47:05 -0400

    Dataset.py: better hdf5 legacy support

--- Dataset.py
+++ Dataset.py
@@ -243,10 +243,15 @@ class Dataset(object):
                 else:
                     address = sorted(list(h5_file.keys()))[0]
                 if verbose >= 1:
-                    wiprint("""Reading sequence DataFrame from '{0}:{1}'
+                    wiprint("""Reading DataFrame from '{0}:{1}'
                             """.format(path, address))
-                values = np.array(h5_file["{0}/values".format(address)])
-                index  = np.array(h5_file["{0}/index".format(address)])
+                if ("{0}/values".format(address) in h5_file
+                and "{0}/index".format(address) in h5_file):
+                    values = np.array(h5_file["{0}/values".format(address)])
+                    index  = np.array(h5_file["{0}/index".format(address)])
+                else:
+                    values = np.array(h5_file[address])
+                    index = np.arange(values.shape[0])
                 attrs  = dict(h5_file[address].attrs)
                 if "fields"  in dataframe_kw:
                     dataframe_kw["columns"] = dataframe_kw.pop("fields")
@@ -256,12 +261,12 @@ class Dataset(object):
                     dataframe_kw["columns"] = list(attrs["fields"])
                 elif "columns" in attrs:
                     dataframe_kw["columns"] = list(attrs["columns"])
-
-                columns = dataframe_kw.pop("columns")
-                columns = map(eval, columns)
-                if np.array([isinstance(c, tuple) for c in columns]).all():
-                    columns = pd.MultiIndex.from_tuples(columns)
-                dataframe_kw["columns"] = columns
+                if "columns" in dataframe_kw:
+                    columns = dataframe_kw.pop("columns")
+                    columns = map(str, columns)
+                    if np.array([isinstance(c, tuple) for c in columns]).all():
+                        columns = pd.MultiIndex.from_tuples(columns)
+                    dataframe_kw["columns"] = columns
                 df = pd.DataFrame(data=values, index=index, **dataframe_kw)
                 if "index_name" in attrs:
                     df.index.name = attrs["index_name"]
